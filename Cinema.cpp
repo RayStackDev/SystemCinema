e58f2f4b295bbdc3a3c2b436564e2d8dcf5b3367
Raymond <raymondlugo64@gmail.com>
2025-07-24 22:29:59 -0400

    Primeiro commit

---
+++ Cinema.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+#include <locale.h>
+using namespace std;
+
+char poltronas1[30][30], poltronas2[30][30], poltronas3[30][30];
+int filme, val, linha[3], coluna[3];
+
